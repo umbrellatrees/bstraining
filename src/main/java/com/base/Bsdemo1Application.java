6b3435e42e11fcf456f87c923e9a27e6c624b546
ppietr <ppietr@poczta.fm>
2016-05-10 19:40:20 +0200

    generate sprint boot project

---
+++ src/main/java/com/base/Bsdemo1Application.java
@@ -0,0 +1,12 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bsdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bsdemo1Application.class, args);
+	}
+}
