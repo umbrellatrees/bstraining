14cf69c7cea022949429f3c4a8f25c473b9dd00c
ppietr <ppietr@poczta.fm>
2016-05-11 18:21:57 +0200

    remove emails

--- src/main/java/com/base/services/AuthenticationProvider.java
+++ src/main/java/com/base/services/AuthenticationProvider.java
@@ -5,10 +5,10 @@ package com.base.services;
  */
 public class AuthenticationProvider {
 
-    public final static String AUTH_TOKEN= "84b3b07be226a16e320e379927f7cde4b58a73f7cd61ac79c449b55c556bb73e";
+    public final static String AUTH_TOKEN= "";
 
-    public final static String SALES_EMAIL= "ppietr@poczta.fm";
+    public final static String SALES_EMAIL= "";
 
-    public final static String ACCOUNT_EMAIL = "pawel.pietroniuk@solidbrain.pl";
+    public final static String ACCOUNT_EMAIL = "";
 
 }
