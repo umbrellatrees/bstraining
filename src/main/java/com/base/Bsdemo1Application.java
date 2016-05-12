b8539d7b691cf7a082a82d4d5dd52718b8c7e7ec
ppietr <ppietr@poczta.fm>
2016-05-12 22:29:55 +0200

    Milestone #4

--- src/main/java/com/base/Bsdemo1Application.java
+++ src/main/java/com/base/Bsdemo1Application.java
@@ -4,12 +4,18 @@ import com.getbase.Client;
 import com.getbase.Configuration;
 import com.getbase.services.LeadsService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class Bsdemo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Bsdemo1Application.class, args);
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(Bsdemo1Application.class);
 	}
 }
